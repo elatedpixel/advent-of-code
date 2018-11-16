71546128492f0ae633c93b1d4d87596e6f73c0f0
David Lynch <elatedpixel@gmail.com>
2018-11-16 17:19:09 -0500

    added day 01 part b

--- 2017/01/src/core.clj
+++ 2017/01/src/core.clj
@@ -7,10 +7,19 @@
         (recur (quot n 10) (conj r (rem n 10))))))
 
 (defn sum-matching-pairs [s]
-  (let [n (parse-numbers s)] 
+  (let [n (parse-numbers s)]
     (->> n
          (partition 2 1 (take 1 n))
          (reduce (fn [r [a b]] (+ r (if (= a b) a 0))) 0))))
 
+(defn sum-matching-circle-pairs [s]
+  (let [n (parse-numbers s)
+        l (count s)]
+    (->> (map vector n (drop (bit-shift-right l 1) (cycle n)))
+         (reduce (fn [r [a b]] (+ r (if (= a b) a 0))) 0))))
+
 (defn -main []
-  (println (sum-matching-pairs (slurp "./input"))))
+  (println (str "Part A: "
+                (sum-matching-pairs (slurp "./input"))))
+  (println (str "Part B: "
+                (sum-matching-circle-pairs (slurp "./input")))))
--- 2017/01/src/test.clj
+++ 2017/01/src/test.clj
@@ -5,6 +5,13 @@
 (deftest test-sum-matching-pairs
   (testing "sum-matching-pairs"
     (testing "works with even, matching pairs"
-      (is (= 4 (sum-matching-pairs "1111"))))))
+      (is (= 4 (sum-matching-pairs "1111"))))
+    (testing "works with uneven pair - first and last matches"
+      (is (= 5 (sum-matching-pairs "2124332"))))))
+
+(deftest test-sum-matching-circle-pairs
+  (testing "sum-matching-circle-pairs"
+    (testing "works with even number"
+      (is (= 6 (sum-matching-circle-pairs "1212"))))))
 
 (run-tests)
