8d9fca50e47a781cdbceb43d5e6bfc81699a4d54
David Lynch <elatedpixel@gmail.com>
2018-12-03 18:49:23 -0500

    quick refactor to reuse code from part 1

--- 2018/src/day3/core.clj
+++ 2018/src/day3/core.clj
@@ -12,25 +12,29 @@
   (is (= [1 1 3 4 4] (parse-plan "#1 @ 1,3: 4x4")))
   (is (= [12 123 304 40 154] (parse-plan "#12 @ 123,304: 40x154"))))
 
+(defn- build-map [plans]
+  (reduce (fn [m [elf col row width height]]
+            (reduce (fn [m [c r]] (update m [c r] #(conj % elf))) m
+                    (for [c (range col (+ col width))
+                          r (range row (+ row height))]
+                      [c r])))
+          {}
+          plans))
+
 (with-test
 
-  (defn find-overlapping [plans]
-    (count (filter (fn [[k v]] (> (count v) 1))
-            (reduce (fn [m [elf col row width height]]
-                      (reduce (fn [m [c r]] (update m [c r] #(conj % elf))) m
-                              (for [c (range col (+ col width))
-                                    r (range row (+ row height))]
-                                [c r])))
-                    {}
-                    plans))))
+  (defn find-overlapping [m]
+    (count (filter (fn [[k v]] (> (count v) 1)) m)))
 
-  (is (= 4 (find-overlapping  (map parse-plan ["#1 @ 1,3: 4x4"
-                                               "#2 @ 3,1: 4x4"
-                                               "#3 @ 5,5: 2x2"])))))
+  (is (= 4 (find-overlapping
+            (build-map (map parse-plan ["#1 @ 1,3: 4x4"
+                                        "#2 @ 3,1: 4x4"
+                                        "#3 @ 5,5: 2x2"]))))))
 
 (run-tests 'day3.core)
 
 (defn -main [file-name]
-  (let [input (map parse-plan (clojure.string/split-lines (slurp file-name)))]
+  (let [input (map parse-plan (clojure.string/split-lines (slurp file-name)))
+        m (build-map input)]
     (prn)
-    (println (str "Day 3 Part 1: " (time (find-overlapping input))))))
+    (println (str "Day 3 Part 1: " (time (find-overlapping m))))))
