a0bf0c5e36314a89846e6bd75a0ac61d8bcc5451
David Lynch <elatedpixel@gmail.com>
2018-12-02 22:54:36 -0500

    day 2 part 2, woo

--- 2018/src/day2/core.clj
+++ 2018/src/day2/core.clj
@@ -17,9 +17,43 @@
                          "abcdee"
                          "ababab"]))))
 
+(t/with-test
+
+  (defn diff [s1 s2]
+    (loop [pairs  (map vector s1 s2)
+           same []
+           d 0]
+      (if (empty? pairs) [d (apply str same)]
+          (let [[a b] (first pairs)]
+            (recur (rest pairs)
+                   (if (= a b) (conj same a) same)
+                   (+ d (if (= a b) 0 1)))))))
+
+  (t/is (= [1 "fgij"] (diff "fghij" "fguij")))
+  (t/is (= [4 "y"] (diff "axcye" "wvxyz"))))
+
+(t/with-test
+
+  (defn checksum-common [coll]
+    (second (apply min-key
+                   (fn [[d v]] d)
+                   (for [[i a] (keep-indexed vector coll)
+                         [j b] (keep-indexed vector coll)
+                         :when (not= i j)]
+                     (diff a b)))))
+
+  (t/is (= "fgij" (checksum-common ["abcde"
+                                    "fghij"
+                                    "klmno"
+                                    "pqrst"
+                                    "fguij"
+                                    "axcye"
+                                    "wvxyz"]))))
+
 (t/run-tests 'day2.core)
 
 (defn -main [input-file]
   (let [input (clojure.string/split-lines (slurp input-file))]
     (prn)
-    (println (str "Day 2 Part 1: " (time (checksum input))))))
+    (println (str "Day 2 Part 1: " (time (checksum input))))
+    (println (str "Day 2 Part 2: " (time (checksum-common input))))))
