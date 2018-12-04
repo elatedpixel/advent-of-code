87bef41cf07303e303d1a4a7f38a73963e0f508e
David Lynch <elatedpixel@gmail.com>
2018-12-03 19:08:56 -0500

    day 3 part 2 done!

--- 2018/src/day3/core.clj
+++ 2018/src/day3/core.clj
@@ -31,10 +31,22 @@
                                         "#2 @ 3,1: 4x4"
                                         "#3 @ 5,5: 2x2"]))))))
 
+(with-test
+
+  (defn find-non-overlapping [m]
+    (let [overlapped (reduce (fn [a [k v]] (if (> (count v) 1) (apply conj a v) a)) #{} m)]
+      (first (val (first (filter (fn [[k [v & _]]] (not (overlapped v))) m))))))
+
+  (is (= 3 (find-non-overlapping
+            (build-map (map parse-plan ["#1 @ 1,3: 4x4"
+                                        "#2 @ 3,1: 4x4"
+                                        "#3 @ 5,5: 2x2"]))))))
+
 (run-tests 'day3.core)
 
 (defn -main [file-name]
   (let [input (map parse-plan (clojure.string/split-lines (slurp file-name)))
         m (build-map input)]
     (prn)
-    (println (str "Day 3 Part 1: " (time (find-overlapping m))))))
+    (println (str "Day 3 Part 1: " (time (find-overlapping m))))
+    (println (str "Day 3 Part 2: " (time (find-non-overlapping m))))))
