ddacb4e4811ca9e4730f973eb1759dfadbfa5de8
Lynch the Lionheart <david.w.lynch-jr@boeing.com>
2019-12-02 12:55:22 -0500

    refactored multimethod to opcode map

--- src/advent_2019/day2.clj
+++ src/advent_2019/day2.clj
@@ -14,35 +14,38 @@
 (defn input->map [d]
   (zipmap (range) d))
 
-(defmulti compute :opcode)
-(defmethod compute 1 [{:keys [args program]}] (transduce (map #(get program %)) + args))
-(defmethod compute 2 [{:keys [args program]}] (transduce (map #(get program %)) * args))
+(def opcode
+  {1 +
+   2 *})
+
+(defn sort-by-keys [m]
+  (map m (sort (keys m))))
 
 (t/with-test
 
   (defn computer [intcode]
     (loop [i 0
            program intcode]
-      (let [[op a b r] (nth (partition-all 4 4 (map program (sort (keys program)))) i)]
-        (if (= 99 op) (vals program)
-            (recur (inc i) (assoc program r (compute {:opcode op :args [a b] :program program})))))))
+      (let [[op a b r] (nth (partition-all 4 4 (sort-by-keys program)) i)]
+        (if (= 99 op) program
+            (recur (inc i) (assoc program r ((opcode op) (program a) (program b))))))))
 
-  (t/is (= [2 0 0 0 99] (computer (input->map [1 0 0 0 99]))))
-  (t/is (= [2 3 0 6 99] (computer (input->map [2 3 0 3 99]))))
-  (t/is (= [2 4 4 5 99 9801] (computer (input->map [2 4 4 5 99 0]))))
-  (t/is (= [30 1 1 4 2 5 6 0 99] (computer (input->map [1 1 1 4 99 5 6 0 99])))))
+  (t/is (= [2 0 0 0 99] (sort-by-keys (computer (input->map [1 0 0 0 99])))))
+  (t/is (= [2 3 0 6 99] (sort-by-keys (computer (input->map [2 3 0 3 99])))))
+  (t/is (= [2 4 4 5 99 9801] (sort-by-keys (computer (input->map [2 4 4 5 99 0])))))
+  (t/is (= [30 1 1 4 2 5 6 0 99] (sort-by-keys (computer (input->map [1 1 1 4 99 5 6 0 99]))))))
 
 (t/run-tests 'advent-2019.day2)
 
 (defn -main []
-  (time (println (computer (-> input
-                               input->map
-                               (assoc 1 12)
-                               (assoc 2 2)))))
-  (time (println (filter #(= 19690720 ((comp first second) %))
-                         (for [a (range 100) b (range 100)]
-                           [(+ b (* 100 a))
-                            (computer (-> input
-                                          input->map
-                                          (assoc 1 a)
-                                          (assoc 2 b)))])))))
+  (time (println ((computer (-> input
+                                input->map
+                                (assoc 1 12)
+                                (assoc 2 2))) 0)))
+  (time (println (ffirst (filter #(= 19690720 ((second %) 0))
+                          (for [a (range 100) b (range 100)]
+                            [(+ b (* 100 a))
+                             (computer (-> input
+                                           input->map
+                                           (assoc 1 a)
+                                           (assoc 2 b)))]))))))
