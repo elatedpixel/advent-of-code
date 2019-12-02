e4d58fb507e296df436da225c2189a4a4b287b0a
Lynch the Lionheart <david.w.lynch-jr@boeing.com>
2019-12-02 14:39:52 -0500

    refactored for speeeeeeeeeeeeeeeeeeeeeed

--- src/advent_2019/day2.clj
+++ src/advent_2019/day2.clj
@@ -23,17 +23,27 @@
 
 (t/with-test
 
-  (defn computer [intcode]
-    (loop [i 0
-           program intcode]
-      (let [[op a b r] (nth (partition-all 4 4 (sort-by-keys program)) i)]
-        (if (= 99 op) program
-            (recur (inc i) (assoc program r ((opcode op) (program a) (program b))))))))
+  (defn computer
+    ([program] (reduce computer program (range)))
+    ([program i]
+     (let [[op a b r] (nth (partition-all 4 4 (sort-by-keys program)) i)]
+       (if (= 99 op)
+         (reduced program)
+         (assoc program r ((opcode op) (program a) (program b)))))))
 
   (t/is (= [2 0 0 0 99] (sort-by-keys (computer (input->map [1 0 0 0 99])))))
   (t/is (= [2 3 0 6 99] (sort-by-keys (computer (input->map [2 3 0 3 99])))))
   (t/is (= [2 4 4 5 99 9801] (sort-by-keys (computer (input->map [2 4 4 5 99 0])))))
   (t/is (= [30 1 1 4 2 5 6 0 99] (sort-by-keys (computer (input->map [1 1 1 4 99 5 6 0 99]))))))
+
+(defn part2 [m]
+  (some #(if (= 19690720 ((second %) 0)) (first %))
+        (pmap (fn [[a b]]
+                [(+ b (* 100 a))
+                 (computer (-> m
+                               (assoc 1 a)
+                               (assoc 2 b)))])
+              (for [a (range 100) b (range 100)] [a b]))))
 
 (t/run-tests 'advent-2019.day2)
 
@@ -42,10 +52,4 @@
                                 input->map
                                 (assoc 1 12)
                                 (assoc 2 2))) 0)))
-  (time (println (ffirst (filter #(= 19690720 ((second %) 0))
-                          (for [a (range 100) b (range 100)]
-                            [(+ b (* 100 a))
-                             (computer (-> input
-                                           input->map
-                                           (assoc 1 a)
-                                           (assoc 2 b)))]))))))
+  (time (println (part2 (input->map input)))))
