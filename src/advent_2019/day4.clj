b3197c743f175b0bfa2c1496a397f7ebab6dc861
Lynch the Lionheart <david.w.lynch-jr@boeing.com>
2019-12-04 10:01:01 -0500

    2019 day 4 part 2

--- src/advent_2019/day4.clj
+++ src/advent_2019/day4.clj
@@ -6,16 +6,28 @@
 (defn six-digit-number? [n]
   (= 6 (count (str n))))
 
-(defn contains-congruent-duplicate? [n]
+(defn contains-contiguous-duplicate? [n]
   (some (fn [[a b]] (= a b)) (partition 2 1 (str n))))
+
+(defn contains-contiguous-unique-duplicate? [n]
+  (let [counts (frequencies (str n))]
+    (some (fn [[a b]] (and (= a b) (= 2 (counts a)))) (partition 2 1 (str n)))))
 
 (defn increasing-order? [n]
   (apply <= (map int (str n))))
 
-(def valid-password?
+(def valid-password-part1?
   (every-pred six-digit-number?
-              contains-congruent-duplicate?
+              contains-contiguous-duplicate?
+              increasing-order?))
+
+(def valid-password-part2?
+  (every-pred six-digit-number?
+              contains-contiguous-unique-duplicate?
               increasing-order?))
 
 (defn part1 []
-  (time (println (count (filter valid-password? input)))))
+  (time (println (count (filter valid-password-part1? input)))))
+
+(defn part2 []
+  (time (println (count (filter valid-password-part2? input)))))
