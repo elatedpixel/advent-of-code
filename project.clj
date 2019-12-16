399bf4e34ff2f817b9ea34101f6a4991259d5d9b
Lynch the Lionheart <elatedpixel@gmail.com>
2019-12-15 22:15:21 -0500

    added combinatorics

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/test.check "0.9.0"]
+                 [org.clojure/math.combinatorics "0.1.6"]
                  [clj-time "0.15.1"]])
--- src/advent_2019/intcode.clj
+++ src/advent_2019/intcode.clj
@@ -4,37 +4,42 @@
 (defn instruction [n]
   [(rem n 100) (vec (reverse (format "%03d" (quot n 100))))])
 
+(defn value-by-mode [program index mode]
+  (case mode
+    \0 (program (program index))
+    \1 (program index)))
+
 (defn execute [{:keys [program pointer input] :as state}]
   (let [[op modes] (instruction (program pointer))
-        value      (fn [i m] (program (if (= \0 m) (program i) i)))]
+        value      (fn [i m] (value-by-mode program i m))]
     (case op
-      1  (-> (assoc-in state [:program (program (+ 3 pointer))]
-                       (+ (value (+ 1 pointer) (modes 0))
-                          (value (+ 2 pointer) (modes 1))))
-             (update :pointer + 4))
-      2  (-> (assoc-in state [:program (program (+ 3 pointer))]
-                       (* (value (+ 1 pointer) (modes 0))
-                          (value (+ 2 pointer) (modes 1))))
-             (update :pointer + 4))
-      3  (-> (assoc-in state [:program (value (+ 1 pointer) (modes 0))] (first input))
-             (assoc :input (vec (rest input)))
-             (update :pointer + 2))
-      4  (-> (assoc state :output (value (+ 1 pointer) (modes 0)))
-             (update :pointer + 2))
-      5  (-> (assoc state :pointer (if ((complement zero?) (value (+ pointer 1) (modes 0)))
-                                     (value (+ pointer 2) (modes 1))
-                                     (+ 3 pointer))))
-      6  (-> (assoc state :pointer (if (zero? (value (+ pointer 1) (modes 0)))
-                                     (value (+ pointer 2) (modes 1))
-                                     (+ 3 pointer))))
-      7  (-> (assoc-in state [:program (program (+ 3 pointer))]
-                       (if (< (value (+ 1 pointer) (modes 0))
-                              (value (+ 2 pointer) (modes 1))) 1 0))
-             (update :pointer + 4))
-      8  (-> (assoc-in state [:progam (program (+ 3 pointer))]
-                       (if (= (value (+ 1 pointer) (modes 0))
-                              (value (+ 2 pointer) (modes 1))) 1 0))
-             (update :pointer + 4))
+      1 (-> (assoc-in state [:program (program (+ 3 pointer))]
+                      (+ (value (+ 1 pointer) (modes 0))
+                         (value (+ 2 pointer) (modes 1))))
+            (update :pointer + 4))
+      2 (-> (assoc-in state [:program (program (+ 3 pointer))]
+                      (* (value (+ 1 pointer) (modes 0))
+                         (value (+ 2 pointer) (modes 1))))
+            (update :pointer + 4))
+      3 (-> (assoc-in state [:program (value (+ 1 pointer) (modes 0))] (first input))
+            (assoc :input (vec (rest input)))
+            (update :pointer + 2))
+      4 (-> (assoc state :output (value (+ 1 pointer) (modes 0)))
+            (update :pointer + 2))
+      5 (-> (assoc state :pointer (if ((complement zero?) (value (+ pointer 1) (modes 0)))
+                                    (value (+ pointer 2) (modes 1))
+                                    (+ 3 pointer))))
+      6 (-> (assoc state :pointer (if (zero? (value (+ pointer 1) (modes 0)))
+                                    (value (+ pointer 2) (modes 1))
+                                    (+ 3 pointer))))
+      7 (-> (assoc-in state [:program (program (+ 3 pointer))]
+                      (if (< (value (+ 1 pointer) (modes 0))
+                             (value (+ 2 pointer) (modes 1))) 1 0))
+            (update :pointer + 4))
+      8 (-> (assoc-in state [:progam (program (+ 3 pointer))]
+                      (if (= (value (+ 1 pointer) (modes 0))
+                             (value (+ 2 pointer) (modes 1))) 1 0))
+            (update :pointer + 4))
       (assoc state :halt? true))))
 
 (t/with-test
