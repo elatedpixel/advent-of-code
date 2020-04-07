00c9665bf4511f80dc017e6ca6fa0f7104d47bac
Lynch the Lionheart <david.w.lynch-jr@boeing.com>
2020-04-07 12:01:40 -0400

    day 2 finished, core async unnecessary until day 5

--- src/advent_2019/day2_async.clj
+++ src/advent_2019/day2_async.clj
@@ -1,5 +1,6 @@
 (ns advent-2019.day2-async
   (:require [clojure.java.io :as io]
+            [clojure.test :as t]
             [clojure.core.async
              :as async
              :refer (chan go go-loop >! <! >!! <!!)]))
@@ -23,42 +24,75 @@
 (defn arguments [program ptr op]
   (let [indexes (iterate inc ptr)]
     (case op
-      1 (vals (select-keys program (rest (take 4 indexes)))))))
+      (1 2) (vals (select-keys program (rest (take 4 indexes))))
+      99    '())))
 
-(defn intcode [{:keys [program ptr in]} input]
+(defn intcode [{:keys [program ptr in out halted?]}]
   (let [op   (get @program @ptr)
         args (arguments @program @ptr op)]
     (case op
       1 (let [[a b c] args]
           (dosync
-           (alter program assoc c (+ a b))
-           (alter ptr + (inc (count args))))))))
+           (alter program assoc c (+ (@program a) (@program b)))
+           (alter ptr + (inc (count args)))))
+      2 (let [[a b c] args]
+          (dosync
+           (alter program assoc c (* (@program a) (@program b)))
+           (alter ptr + (inc (count args)))))
+      99 (dosync (ref-set halted? true)
+                 (alter ptr inc)))))
 
 (defprotocol Computer
   (run [this]))
 
-(defrecord Intcode [program ptr in out]
+(defrecord Intcode [program ptr in out halted?]
   Computer
   (run [this]
-    #_(intcode this 0)
-    (go
-        (>! (:out this) (intcode this (<! (:in this)))))))
+    (while (not @(:halted? this))
+      (intcode this))
+    this))
 
 (defn make-intcode
   [intcode & {:keys [ptr in out]
               :or   {ptr 0
                      in  (chan)
                      out (chan)}}]
-  (->Intcode (ref intcode) (ref ptr) in out))
+  (->Intcode (ref intcode) (ref ptr) in out (ref false)))
+
+(t/deftest test-intcode
+  (t/are [input expect] (= expect
+                           (-> input
+                               input->map
+                               make-intcode
+                               run
+                               :program
+                               deref
+                               sort-by-keys))
+    [1 0 0 0 99] [2 0 0 0 99]
+    [2 3 0 3 99] [2 3 0 6 99]
+    [2 4 4 5 99 0] [2 4 4 5 99 9801]
+    [1 1 1 4 99 5 6 0 99] [30 1 1 4 2 5 6 0 99])
+                                        ;
+  )
 
 (comment
                                         ;
-  (def computer-a (make-intcode input))
+  (def computer-a (make-intcode (-> input
+                                    (assoc 1 12)
+                                    (assoc 2 2))))
                                         ;
   (run computer-a)
+
+  ((:program computer-a) 0)
                                         ;
   (>!! (:in computer-a) 0)
                                         ;
   (println (<!! (:out computer-a)))
                                         ;
+  (for [noun (range 100)
+        verb (range 100)
+        :when (= 19690720
+                 ((:program (run (make-intcode (-> input (assoc 1 noun) (assoc 2 verb))))) 0))]
+    [noun verb]
+    )
   )
