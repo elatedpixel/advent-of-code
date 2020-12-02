83c57e48a3b9fd4b9ebebab77065ab776aad5215
Lynch the Lionheart <elatedpixel@gmail.com>
2020-12-02 15:40:58 -0500

    updated performance for day 1 from the insight that the answer will be to the left side of sorted input data

--- src/advent_2020/day01.clj
+++ src/advent_2020/day01.clj
@@ -1,5 +1,6 @@
 (ns advent-2020.day01
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.math.combinatorics :as combo]))
 
 (def input
   (into #{}
@@ -8,27 +9,30 @@
 
 (comment
   (time
-   (take 1
-         (for [a     input
-               :let  [b (- 2020 a)]
-               :when (and (not= a b) (boolean (input b)))]
-           (* a b))))
+   (println
+    (take 1
+          (for [a     (sort input)
+                :let  [b (- 2020 a)]
+                :when (and (not= a b) (boolean (input b)))]
+            (* a b)))))
 
-  "Elapsed time: 0.360913 msecs"
+  "Elapsed time: 1.179171 msecs"
   ;; => (970816)
                                         ;part 1
   )
 
 (comment
   (time
-   (take 1
-         (for [a     input
-               b     input
-               :let  [c (- 2020 a b)]
-               :when (and (not= a b c) (boolean (input c)))]
-           (* a b c))))
+   (println
+    (let [sorted-input (sort input)]
+      (take 1
+            (for [a     sorted-input
+                  b     sorted-input
+                  :let  [c (- 2020 a b)]
+                  :when (and (not= a b c) (boolean (input c)))]
+              (* a b c))))))
 
-  "Elapsed time: 0.248882 msecs"
+  "Elapsed time: 1.219552 msecs"
   ;; => (96047280)
                                         ;part 2
   )
