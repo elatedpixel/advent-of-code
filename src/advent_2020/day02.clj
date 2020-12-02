ba74aef90d10ee1de79849141b3be1295bc7c2a4
Lynch the Lionheart <elatedpixel@gmail.com>
2020-12-02 12:16:41 -0500

    refactored to separate parsing from validation

--- src/advent_2020/day02.clj
+++ src/advent_2020/day02.clj
@@ -1,34 +1,37 @@
 (ns advent-2020.day02
   (:require [clojure.java.io :as io]))
 
-(defn valid-password? [test-fn s]
-  (let [regex #"(\d+)-(\d+) ([a-z]): (\w+)"]
-    (test-fn (rest (re-matches regex s)))))
+(defn- parse [s]
+  (rest (re-matches #"(\d+)-(\d+) ([a-z]): (\w+)" s)))
 
-(def input
-  (line-seq (io/reader (io/resource "2020/day02"))))
+(def ^:private input
+  (sequence
+   (map parse)
+   (line-seq (io/reader (io/resource "2020/day02")))))
+
+(defn count-valid-passwords [test-fn passwords]
+  (time (println (count (filter test-fn passwords)))))
 
 (comment
                                         ;part 1
-  (letfn [(test-password [[from to letter password]]
+  (letfn [(valid-password? [[from to letter password]]
+            "a valid `password` has [`from`-`to`] occurrences of `letter`"
             (<= (Integer/parseInt from)
                 (get (frequencies password) (first letter) 0)
                 (Integer/parseInt to)))]
-    (time (println (count (filter (partial valid-password? test-password) input)))))
+    (count-valid-passwords valid-password? input))
   ;; 660
   ;; "Elapsed time: 10.796568 msecs"
-                                        ;
-  )
 
-(comment
                                         ;part 2
-  (letfn [(test-password [[index-a index-b letter password]]
+  (letfn [(valid-password? [[index-a index-b letter password]]
+            "a valid `password` has `letter` at `index-a` or `index-b`"
             (let [a (get password (dec (Integer/parseInt index-a)))
                   b (get password (dec (Integer/parseInt index-b)))
                   c (first letter)]
               (and (or (= a c) (= b c))
                    (not= a b))))]
-    (time (println (count (filter (partial valid-password? test-password) input)))))
+    (count-valid-passwords valid-password? input))
   ;; 530
   ;; "Elapsed time: 4.24681 msecs"
                                         ;
