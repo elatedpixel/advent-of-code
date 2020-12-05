8fee62f56d91436cb575295a047b943266cc7a39
Lynch the Lionheart <elatedpixel@gmail.com>
2020-12-05 00:15:31 -0500

    day 4 spec

--- src/advent_2020/day04.clj
+++ src/advent_2020/day04.clj
@@ -15,11 +15,26 @@
 (def input
   (map parse-fields (s/split (slurp (io/resource "2020/day04")) #"\n\n")))
 
-(spec/def ::passport-part1
+(spec/def ::passport
   (spec/keys :req-un [::ecl ::byr ::iyr ::hgt ::pid ::hcl ::eyr]
-             :opt [::cid]))
+             :opt-un [::cid]))
 
+;; part 1
 (comment
-  ;; part 1
-  (count (filter #(spec/valid? ::passport-part1 %) input)))
+  (println (count (filter #(spec/valid? ::passport %) input))))
 
+(spec/def ::byr #(<= 1920 (Integer/parseInt %) 2002))
+(spec/def ::iyr #(<= 2010 (Integer/parseInt %) 2020))
+(spec/def ::eyr #(<= 2020 (Integer/parseInt %) 2030))
+(spec/def ::hgt #(let [[matched? height units] (re-matches #"(\d+)(cm|in)" %)]
+                   (and matched?
+                        (case units
+                          "cm" (<= 150 (Integer. height) 193)
+                          "in" (<= 59 (Integer. height) 76)))))
+(spec/def ::hcl #(re-matches #"\#[0-9a-f]{6}" %))
+(spec/def ::ecl #{"brn" "oth" "hzl" "gry" "blu" "amb" "grn"})
+(spec/def ::pid #(re-matches #"\d{9}" %))
+
+;; part 2
+(comment
+  (println (count (filter #(spec/valid? ::passport %) input))))
