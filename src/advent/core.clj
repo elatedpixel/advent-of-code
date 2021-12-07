28809776c8e39eaddd12922b4b3024dcf0965b0e
Lynch the Lionheart <elatedpixel@gmail.com>
2021-12-07 10:23:04 -0500

    added some statistical functions

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -8,6 +8,33 @@
 (defn string->sexpression
   [s] (read-string (str "(" s ")")))
 
+(defn mean [coll]
+  (let [sum (apply + coll)
+        count (count coll)]
+    (if (pos? count)
+      (/ sum count)
+      0)))
+
+(defn median [coll]
+  (let [sorted (sort coll)
+        cnt (count sorted)
+        halfway (quot cnt 2)]
+    (if (odd? cnt)
+      (nth sorted halfway) ; (1)
+      (let [bottom (dec halfway)
+            bottom-val (nth sorted bottom)
+            top-val (nth sorted halfway)]
+        (mean [bottom-val top-val])))))
+
+(defn mode [coll]
+  (let [freqs (frequencies coll)
+        occurrences (group-by val freqs)
+        modes (last (sort occurrences))
+        modes (->> modes
+                   val
+                   (map key))]
+    modes))
+
 (defn key-by [f selector] (fn [m] (key (apply f selector m))))
 
 (defprotocol ContextMachine
