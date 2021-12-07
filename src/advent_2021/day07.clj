468e6d5194b4a216ecc29046f45e20d02e14d658
Lynch the Lionheart <elatedpixel@gmail.com>
2021-12-07 10:22:34 -0500

    2021.07

---
+++ src/advent_2021/day07.clj
@@ -0,0 +1,25 @@
+(ns advent-2021.day07
+  (:require [advent.core :refer (load-input string->sexpression)]))
+
+(def example (string->sexpression "16,1,2,0,4,2,7,1,2,14"))
+
+(def input (string->sexpression (first (load-input 2021 7))))
+
+(defn sum [xs] (reduce +' xs))
+
+(defn delta [x]
+  (fn [y] (Math/abs (- x y))))
+
+(defn gold-fuel-cost [x]
+  (/ (* x (inc x)) 2))
+
+(defn align [xs fuel-cost]
+  (apply min-key second
+        (for [i (range 1000)]
+          (list i (sum (map (comp fuel-cost (delta i)) xs))))))
+
+(align input identity)
+;; => (361 354129)
+
+(align input gold-fuel-cost)
+;; => (494 98905973)
