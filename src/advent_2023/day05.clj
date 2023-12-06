f4a2dee16215dfe61094d4d342c61f118d321b4a
Lynch the Lionheart <elatedpixel@gmail.com>
2023-12-06 11:07:16 -0500

    5.1, merging ranges was a bad idea but pairs of (pred, f) is fast

--- src/advent_2023/day05.clj
+++ src/advent_2023/day05.clj
@@ -2,7 +2,8 @@
   (:require
    [clojure.java.io :as io]
    [clojure.string :as str]
-   [clojure.test :as test]))
+   [clojure.test :as test]
+   [criterium.core :as criterium]))
 
 (def input (str/trim (slurp (io/resource "2023/day05"))))
 (def sample (str/trim (slurp (io/resource "2023/day05.sample"))))
@@ -11,13 +12,18 @@
   [s]
   (mapv read-string (re-seq #"\d+" s)))
 
-(defn- range* [[dst src len]]
-  (into {} (map vector
-                (range src (+ src len))
-                (range dst (+ dst len)))))
+(defn- range*
+  "we need to use this like a pair in a `cond` statement, returns (pred fn)"
+  [[dst src len]]
+  (list
+   (fn [n] (<= src n (dec (+ len src))))
+   (fn [n] (+ n (- dst src)))))
 
 (defn- lookup [r]
-  (fn [x] (get r x x)))
+  (fn [x] (reduce
+           (fn [x [pred f]] (if (pred x) (reduced (f x)) x))
+           x
+           (partition 2 r))))
 
 (defn- resolve* [m]
   (comp
@@ -41,19 +47,23 @@
          :else
          (let [[name-str & ranges-str] (str/split-lines s)
                name                    (keyword (re-find #"[\w-]+" name-str))
-               ranges                  (transduce (map (comp range* to-ints)) merge ranges-str)]
+               ranges                  (mapcat (comp range* to-ints) ranges-str)]
            (assoc m name ranges))))
      {}
      sections)))
-(comment
-  (parse sample))
 
 (defn part-1 [input]
   (let [almanac (parse input)]
     (apply min (map (resolve* almanac) (:seeds almanac)))))
 
-(part-1 input)
-;; => OutOfMemoryError Java heap space  clojure.lang.PersistentHashMap$BitmapIndexedNode.assoc (PersistentHashMap.java:882)
+(criterium/quick-bench (part-1 input))
+;; => 600279879
+;; Evaluation count : 120 in 6 samples of 20 calls.
+;;              Execution time mean : 5.565616 ms
+;;     Execution time std-deviation : 227.994195 µs
+;;    Execution time lower quantile : 5.317659 ms ( 2.5%)
+;;    Execution time upper quantile : 5.791945 ms (97.5%)
+;;                    Overhead used : 16.002083 ns
 
 (test/deftest part1
   (test/is (= 35 (part-1 sample))))
