b6e1ac1638e22d848f762034a3d9ecc66341a3f1
david lynch <davidlynch@davids-MacBook-Air.local>
2024-12-02 13:53:45 -0500

    refactoring names and removing unused imports

--- src/advent_2024/day01.clj
+++ src/advent_2024/day01.clj
@@ -1,34 +1,25 @@
 (ns advent-2024.day01
-  (:require [clojure.test :as t]
-            [clojure.java.io :as io]
-            [clojure.string :as str]
-            [clojure.pprint :as pprint]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
-(defn- distance [a b]
-  (if (< a b)
-    (- b a)
-    (- a b)))
+(defn- delta-abs [a b]
+  (if (< a b) (- b a) (- a b)))
 
 (defn -main []
-  (let [input
-        (slurp (io/resource "2024/day01.txt"))
-        distances
-        (reduce
-         (fn [distances lists-string]
-           (let [[a b] (str/split lists-string #"\s+")]
-             (-> distances
-                 (update :left conj (parse-long a))
-                 (update :right conj (parse-long b)))))
-         {}
-         (str/split-lines input))]
-    (pprint/pprint
-     (reduce #'+
-             (map distance
-                  (sort (:left distances))
-                  (sort (:right distances)))))
-    (pprint/pprint
-     (let [m (frequencies (:right distances))]
-       (reduce #'+
-               0
-               (map (fn [n] (* n (get m n 0)))
-                    (:left distances)))))))
+  (let [input     (slurp (io/resource "2024/day01.txt"))
+        distances (reduce
+                   (fn [distances lists-string]
+                     (let [[a b] (str/split lists-string #"\s+")]
+                       (-> distances
+                           (update :left conj (parse-long a))
+                           (update :right conj (parse-long b)))))
+                   {}
+                   (str/split-lines input))]
+    (println
+     (reduce #'+ (map delta-abs
+                      (sort (:left distances))
+                      (sort (:right distances)))))
+    (println
+     (let [occurrences (frequencies (:right distances))]
+       ;; for each element `n` in the left list, multiply `n` by it's occurrences in the right list, and sum
+       (transduce (map (fn [n] (* n (get occurrences n 0)))) #'+ 0 (:left distances))))))
