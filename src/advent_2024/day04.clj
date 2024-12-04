bae0907b574dba7ab04a07999e2b9b88f2ad7431
david lynch <davidlynch@davids-MacBook-Air.local>
2024-12-04 14:47:39 -0500

    day 4 part 2

--- src/advent_2024/day04.clj
+++ src/advent_2024/day04.clj
@@ -18,36 +18,46 @@
    {}
    (map-indexed vector list-of-strings)))
 
-(let [offsets-list
-      [[[0 0] [0 -1] [0 -2] [0 -3]]
-       [[0 0] [1 -1] [2 -2] [3 -3]]
-       [[0 0] [1 0] [2 0] [3 0]]
-       [[0 0] [1 1] [2 2] [3 3]]
-       [[0 0] [0 1] [0 2] [0 3]]
-       [[0 0] [-1 1] [-2 2] [-3 3]]
-       [[0 0] [-1 0] [-2 0] [-3 0]]
-       [[0 0] [-1 -1] [-2 -2] [-3 -3]]]]
-  (defn- neighbors [grid]
-    (fn [[x y]]
-      (keep (fn [offsets]
-              (let [coords (mapv (fn [[x' y']] [(+ x x') (+ y y')]) offsets)]
-                (when (every? #(grid %) coords)
-                  (apply str (map grid coords)))))
-            offsets-list))))
-
+(defn- neighbors [grid offsets-list]
+  (fn [[x y]]
+    (keep (fn [offsets]
+            (let [coords (mapv (fn [[x' y']] [(+ x x') (+ y y')]) offsets)]
+              (when (every? #(grid %) coords)
+                (apply str (map grid coords)))))
+          offsets-list)))
 
 ;; count occurrences of "XMAS" in word-search grid
 (defn part1 [input]
-  (let [grid (word-search input)]
-    ((frequencies (mapcat (neighbors grid) (keys grid))) "XMAS")))
+  (let [grid (word-search input)
+        offsets-list [[[0 0] [0 -1] [0 -2] [0 -3]]
+                      [[0 0] [1 -1] [2 -2] [3 -3]]
+                      [[0 0] [1 0] [2 0] [3 0]]
+                      [[0 0] [1 1] [2 2] [3 3]]
+                      [[0 0] [0 1] [0 2] [0 3]]
+                      [[0 0] [-1 1] [-2 2] [-3 3]]
+                      [[0 0] [-1 0] [-2 0] [-3 0]]
+                      [[0 0] [-1 -1] [-2 -2] [-3 -3]]]]
+    ((frequencies (mapcat (neighbors grid offsets-list) (keys grid))) "XMAS")))
 
 (t/deftest test-part1
   (t/is (= 18 (part1 sample0))))
 
-(defn part2 [input] 0)
+(defn part2 [input]
+  (let [grid (word-search input)
+        offsets-list [[[-1 -1] [0 0] [1 1]]
+                      [[-1 1] [0 0] [1 -1]]]]
+    (count
+     (sequence (comp
+                (map (neighbors grid offsets-list))
+                (filter (fn [[a b]] (and (or (= a "MAS")
+                                             (= a "SAM"))
+                                         (or (= b "MAS")
+                                             (= b "SAM"))))))
+               (keys grid)))))
 
 (t/deftest test-part2
-  (t/is (= 0 (part2 sample0))))
+  (t/is (= 9 (part2 sample0))))
 
 (defn -main []
-  (println (str "Day 4 Part 1: " (part1 puzzle))))
+  (println (str "Day 4 Part 1: " (part1 puzzle)))
+  (println (str "Day 4 Part 2: " (part2 puzzle))))
