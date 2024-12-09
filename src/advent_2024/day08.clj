28c4d480fcb2e874729ba3a09cd8dbd5b2687d72
david lynch <davidlynch@davids-MacBook-Air.local>
2024-12-09 17:12:43 -0500

    day 8 done, problem description comprehension was more difficult than solving problem

--- src/advent_2024/day08.clj
+++ src/advent_2024/day08.clj
@@ -8,22 +8,127 @@
        (io/resource)
        (io/reader)
        (line-seq)
-       (map-indexed vector)
-       (reduce
-         (fn [m [y line]]
-           (reduce
-             (fn [m [x c]]
+       vec
+       (reduce-kv
+         (fn [m y line]
+           (reduce-kv
+             (fn [m x c]
                (cond-> m
                  (not= c \.) (update-in [:antenna c] (fnil conj []) [x y])
                  :always     (assoc-in [:grid [x y]] c)))
              m
-             (map-indexed vector line)))
+             (vec line)))
          {})))
 
-(def ^:private sample0
-  (parse "2024/day08.sample0"))
+(def ^:private sample0 (parse "2024/day08.sample0"))
+(def ^:private puzzle (parse "2024/day08.txt"))
 
-(defn part-1 [puzzle] 0)
+(defn- project-slope
+  ([slope]
+   (fn [coord]
+     (mapv + slope coord)))
+  ([a b]
+   (let [slope (mapv - b a)]
+     (mapv + slope b)))
+  ([grid a b]
+   (let [slope (mapv - b a)]
+     (take-while (fn [coord] (some? (grid coord)))
+                 (iterate (project-slope slope) b)))))
+
+(defn- antinodes
+  [{:keys [grid antenna] :as puzzle}]
+  (reduce-kv
+    (fn [m antenna-char antenna-coords]
+      (into
+        m
+        (filter (fn [coord] (some? (grid coord))))
+        (for [a     antenna-coords
+              b     antenna-coords
+              :when (not= a b)]
+          (project-slope a b))))
+    #{}
+    antenna))
+
+(defn- resonant-frequency-antinodes
+  [{:keys [grid antenna] :as puzzle}]
+  (reduce-kv
+    (fn [m antenna-char antenna-coords]
+      (into
+        m
+        (mapcat concat)
+        (for [a     antenna-coords
+              b     antenna-coords
+              :when (not= a b)]
+          (project-slope grid a b))))
+    #{}
+    antenna))
+
+(comment
+  (antinodes sample0)
+  ;; => #{[0 7]
+  ;;      [1 5]
+  ;;      [2 3]
+  ;;      [3 1]
+  ;;      [3 6]
+  ;;      [4 2]
+  ;;      [6 0]
+  ;;      [6 5]
+  ;;      [7 7]
+  ;;      [9 4]
+  ;;      [10 2]
+  ;;      [10 10]
+  ;;      [10 11]
+  ;;      [11 0]}
+
+  (resonant-frequency-antinodes sample0)
+  ;; => #{[0 0]
+  ;;      [0 7]
+  ;;      [1 0]
+  ;;      [1 1]
+  ;;      [1 5]
+  ;;      [1 10]
+  ;;      [2 2]
+  ;;      [2 3]
+  ;;      [2 8]
+  ;;      [3 1]
+  ;;      [3 3]
+  ;;      [3 6]
+  ;;      [3 11]
+  ;;      [4 2]
+  ;;      [4 4]
+  ;;      [4 9]
+  ;;      [5 2]
+  ;;      [5 5]
+  ;;      [5 7]
+  ;;      [6 0]
+  ;;      [6 5]
+  ;;      [6 6]
+  ;;      [7 3]
+  ;;      [7 7]
+  ;;      [8 1]
+  ;;      [8 8]
+  ;;      [9 4]
+  ;;      [9 9]
+  ;;      [10 2]
+  ;;      [10 10]
+  ;;      [10 11]
+  ;;      [11 0]
+  ;;      [11 5]
+  ;;      [11 11]}
+  )
+
+(defn part-1 [puzzle]
+  (count (antinodes puzzle)))
 
 (t/deftest test-part-1
   (t/is (= 14 (part-1 sample0))))
+
+(defn part-2 [puzzle]
+  (count (resonant-frequency-antinodes puzzle)))
+
+(t/deftest test-part-2
+  (t/is (= 34 (part-2 sample0))))
+
+(defn -main []
+  (println (str "Day 8 Part 1: " (part-1 puzzle)))
+  (println (str "Day 8 Part 2: " (part-2 puzzle))))
